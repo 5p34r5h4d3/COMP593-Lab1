bd1327735d742205b3d9c053b3f97cc90a6d2147
5p34r5h4d3 <benjamin.mathew@flemingcollege.ca>
2022-05-29 15:55:39 -0400

    Modified file in clone repo
    deleted print statement
    added 2 more statement

--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 # Same as the previous scrip[t]
 print("Beautiful is better than ugly.")
 print("Explicit is better than implicit.")
+print("Although that way may not be obvious at first unless you're Dutch ")
+print("Too lazy to copy another line")
